78894d6398ba684e3cf236b4e9b0dfcf3bd94b66
HiSunnykr-1812 <Ashutoshrock.18@gmail.com>
2024-01-19 01:14:08 +0530

    lambda expression1

--- src/java8/App.java
+++ src/java8/App.java
@@ -1,10 +1,35 @@
 package java8;
 
+interface Executable {
+	void execute();
+}
+
+class Runner {
+	public void run(Executable e) {
+		System.out.println("Executing code block...");
+		e.execute();
+	}
+
+}
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Runner runner = new Runner();
+		runner.run(new Executable() {
 
+			@Override
+			public void execute() {
+				// TODO Auto-generated method stub
+				System.out.println("Hello there");
+
+			}
+		});
+		System.out.println("******************//Lambda expression****************************");
+		
+		runner.run(()->System.out.println("this is lambda expression"));
+
+	}
 }
